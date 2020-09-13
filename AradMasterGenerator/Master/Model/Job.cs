3f1c159f3d6be524e7e8b35a9b7ba293811ea370
aradtamako <40272766+aradtamako@users.noreply.github.com>
2020-09-13 19:03:04 +0900

    Add JobMaster generator

---
+++ AradMasterGenerator/Master/Model/Job.cs
@@ -0,0 +1,18 @@
+namespace AradMasterGenerator.Master.Model
+{
+    class Job
+    {
+        public string Id { get; set; } = default!;
+        public string GrowId { get; set; } = default!;
+
+        public string NameKor { get; set; } = default!;
+        public string? NameEng { get; set; }
+        public string? NameJpn { get; set; }
+        public string? NameZho { get; set; }
+
+        public string GrowNameKor { get; set; } = default!;
+        public string? GrowNameEng { get; set; }
+        public string? GrowNameJpn { get; set; }
+        public string? GrowNameZho { get; set; }
+    }
+}
--- AradMasterGenerator/Program.cs
+++ AradMasterGenerator/Program.cs
@@ -1,27 +1,50 @@
 using Core.Config;
 using Core.NeopleOpenApi;
+using Core.NeopleOpenApi.Model;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AradMasterGenerator
 {
     class Program
     {
+        static async Task GenerateJobMaster(NeopleOpenApiClient client)
+        {
+            var jobs = new List<Master.Model.Job>();
+            foreach (var job in (await client.GetJobList().ConfigureAwait(false)).Jobs)
+            {
+                foreach (var jobGrow in job.JobGrows ?? default!)
+                {
+                    jobs.Add(new Master.Model.Job
+                    {
+                        Id = job.JobId,
+                        GrowId = jobGrow.JobGrowId,
+                        NameKor = job.JobName,
+                        GrowNameKor = jobGrow.JobName
+                    });
+                }
+            }
+
+            File.WriteAllText("job_master.json", JsonConvert.SerializeObject(jobs, Formatting.Indented));
+        }
+
         static async Task Main()
         {
             var client = new NeopleOpenApiClient(Config.Instance.NeopleOpenApi.ApiKey);
-            var jobs = await client.GetJobList();
-            Console.WriteLine(JsonConvert.SerializeObject(jobs));
+            await GenerateJobMaster(client).ConfigureAwait(false);
+            return;
 
             var jobId = "41f1cdc2ff58bb5fdc287be0db2a8df3";
             var jobGrowId = "df3870efe8e8754011cd12fa03cd275f";
-            var skills = (await client.GetSkillList(jobId, jobGrowId)).Skills;
+            var skills = (await client.GetSkillList(jobId, jobGrowId).ConfigureAwait(false)).Skills;
             Console.WriteLine(JsonConvert.SerializeObject(skills));
 
             foreach (var skill in skills)
             {
-                var skillDetail = await client.GetSkillDetail(jobId, skill.SkillId);
+                var skillDetail = await client.GetSkillDetail(jobId, skill.SkillId).ConfigureAwait(false);
                 Console.WriteLine(JsonConvert.SerializeObject(skillDetail));
             }
         }
