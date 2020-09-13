15ea5e46af8c68e7523e6ab53e9894b49971ae95
aradtamako <40272766+aradtamako@users.noreply.github.com>
2020-09-13 23:51:53 +0900

    Support common skills

--- AradMasterGenerator/Program.cs
+++ AradMasterGenerator/Program.cs
@@ -58,7 +58,47 @@ namespace AradMasterGenerator
         static async Task GenerateSkillMaster(NeopleOpenApiClient neopleOpenApiClient, DnfOfficialWebSiteClient dnfOfficialWebSiteClient)
         {
             var skills = new List<Core.Master.Model.Skill>();
-            var skillIcons = await dnfOfficialWebSiteClient.GetSkillIcons();
+            //var skillIcons = await dnfOfficialWebSiteClient.GetSkillIcons();
+            //File.WriteAllText("lfkjdlkfdjs.json", JsonConvert.SerializeObject(skillIcons));
+            var skillIcons = JsonConvert.DeserializeObject<Core.Master.Model.Skill[]>(File.ReadAllText("lfkjdlkfdjs.json"));
+
+            // 全職業共通スキル
+            var commonSkills = new List<Core.Master.Model.Skill>
+            {
+                // バックステップ
+                new Core.Master.Model.Skill { Id = "7822d6d52e10964a6755f142c666b494", NameKor = "백스텝", RequiredLevel = 1, Type = "active", CostType = "SP" },
+                // クイックスタンディング
+                new Core.Master.Model.Skill { Id = "ce26c6b69d02a440a81b552bec94f03b", NameKor = "퀵 스탠딩", RequiredLevel = 1, Type = "active", CostType = "SP" },
+                // 基本技熟練
+                new Core.Master.Model.Skill { Id = "5a56514f35cf0270ae8d6c65f8fefd78", NameKor = "기본기 숙련", RequiredLevel = 1, Type = "passive", CostType = "SP" },
+                // 跳躍
+                new Core.Master.Model.Skill { Id = "1fea5a626f15230237946a11a9d11582", NameKor = "도약", RequiredLevel = 10, Type = "active", CostType = "SP" },
+                // 物理クリティカルヒット
+                new Core.Master.Model.Skill { Id = "fc1262c19f3d0477ee8eda47b8db8696", NameKor = "물리 크리티컬 히트", RequiredLevel = 20, Type = "passive", CostType = "SP" },
+                // 魔法クリティカルヒット
+                new Core.Master.Model.Skill { Id = "fc1262c19f3d0477ee8eda47b8db8696", NameKor = "마법 크리티컬 히트", RequiredLevel = 20, Type = "passive", CostType = "SP" },
+                // 物理バックアタック
+                new Core.Master.Model.Skill { Id = "", NameKor = "물리 백 어택", RequiredLevel = 20, Type = "passive", CostType = "SP" },
+                // 魔法バックアタック
+                new Core.Master.Model.Skill { Id = "", NameKor = "마법 백 어택", RequiredLevel = 20, Type = "passive", CostType = "SP" },
+                // 古代の記憶
+                new Core.Master.Model.Skill { Id = "de13113fc6cb4c8880e8d985edb34aea", NameKor = "고대의 기억", RequiredLevel = 15, Type = "active", CostType = "SP" },
+                // 不屈の意志
+                new Core.Master.Model.Skill { Id = "", NameKor = "불굴의 의지", RequiredLevel = 15, Type = "active", CostType = "SP" },
+                // 投擲マスタリー
+                new Core.Master.Model.Skill { Id = "", NameKor = "투척 마스터리", RequiredLevel = 10, Type = "passive", CostType = "SP" },
+                // コンバージョン
+                new Core.Master.Model.Skill { Id = "12dca7fbf791e882b025a0d916181673", NameKor = "컨버전", RequiredLevel = 20, Type = "passive", CostType = "SP" },
+                // ホウキコントロール（クリエイター用）
+                new Core.Master.Model.Skill { Id = "5152480fdde81362575a488d4cec4af9", NameKor = "빗자루 다루기", RequiredLevel = 1, Type = "passive", CostType = "SP" },
+            };
+
+            foreach (var commonSkill in commonSkills)
+            {
+                var skillIcon = skillIcons.Where(x => x.NameKor == commonSkill.NameKor).FirstOrDefault();
+                commonSkill.IconUrl = skillIcon.IconUrl;
+                skills.Add(commonSkill);
+            }
 
             foreach (var job in await neopleOpenApiClient.GetJobs().ConfigureAwait(false))
             {
@@ -66,7 +106,7 @@ namespace AradMasterGenerator
                 {
                     for (var nextJobGrow = jobGrow; nextJobGrow != null; nextJobGrow = nextJobGrow.Next)
                     {
-                        foreach (var skill in await neopleOpenApiClient.GetSkills(job.JobId, jobGrow.JobGrowId).ConfigureAwait(false))
+                        foreach (var skill in await neopleOpenApiClient.GetSkills(job.JobId, nextJobGrow.JobGrowId).ConfigureAwait(false))
                         {
                             var skillIcon = skillIcons.Where(x => x.NameKor.Replace(" ", "") == skill.Name.Replace(" ", "")).FirstOrDefault();
 
@@ -79,7 +119,7 @@ namespace AradMasterGenerator
                             {
                                 Id = skill.SkillId,
                                 JobId = job.JobId,
-                                JobGrowId = jobGrow.JobGrowId,
+                                JobGrowId = nextJobGrow.JobGrowId,
                                 RequiredLevel = skill.RequiredLevel,
                                 Type = skill.Type,
                                 CostType = skill.CostType,
